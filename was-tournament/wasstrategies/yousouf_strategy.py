99393637049554219037b51e4059fa1a9dbb50a6
YoucTagh <yousouf.taghzouti@emse.fr>
2022-05-11 11:04:25 +0200

    yousouf strat

---
+++ was-tournament/wasstrategies/yousouf_strategy.py
@@ -0,0 +1,42 @@
+"""Code retrieved from Axelrod rand.py strategy"""
+
+from axelrod.action import Action
+from axelrod.player import Player
+
+C, D = Action.C, Action.D
+
+
+class Yousouf(Player):
+    """
+    A player starts by cooperating and then mimics previous move by
+    opponent.
+
+    Note that the code for this strategy is written in a fairly verbose
+    way. This is done so that it can serve as an example strategy for
+    those who might be new to Python.
+
+    Names
+
+    - Rapoport's strategy: [Axelrod1980]_
+    - TitForTat: [Axelrod1980]_
+    """
+
+    # These are various properties for the strategy
+    name = 'Tit For Tat'
+    classifier = {
+        'memory_depth': 1,  # Four-Vector = (1.,0.,1.,0.)
+        'stochastic': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def strategy(self, opponent):
+        """This is the actual strategy"""
+        # First move
+        if len(self.history) == 0:
+            return C
+        # React to the opponent's last move
+        if opponent.history[-1] == D or opponent.history[-2] == D:
+            return D
+        return C
